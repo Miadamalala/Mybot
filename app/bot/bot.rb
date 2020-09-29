63be82d12b699522e7f7d853dd4b7dc7864780a4
Miadamalala <rammiadamalala@gmai.com>
2020-09-29 11:20:10 +0000

    ENV Acces token

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token,
+  access_token: access_token: ENV["Mybot"],
   subscribed_fields: %w[feed mention name]
 )
 Bot.on :message do |message|
